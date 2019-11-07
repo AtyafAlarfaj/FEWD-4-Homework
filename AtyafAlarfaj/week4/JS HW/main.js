184c1190aa9104672cea515ddeb081c888bd86a1
AtyafAlarfaj <46459314+AtyafAlarfaj@users.noreply.github.com>
2019-11-07 19:37:26 +0300

    JS HW

---
+++ AtyafAlarfaj/week4/JS HW/main.js
@@ -0,0 +1,24 @@
+var number = 1755;
+
+function factorialize(number){
+    
+    if (number == 3)
+    {
+    console.log("Pling");
+    return true;
+    }
+    else if (number == 5)
+    {
+        console.log("Plang");
+        return true;
+    }
+    else if (number == 7)
+    {
+        console.log("Plong");
+        return true;
+    }
+    else{
+        console.log('$number');
+        return true;
+    }
+}
